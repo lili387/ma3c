348787d2a4e72385872d5f3dbdc4779d46d4505e
Linus <linus.lind@tabyenskilda.se>
2020-01-16 10:55:09 +0100

    fler frågor numerisk integration

--- numint.py
+++ numint.py
@@ -17,3 +17,7 @@ print(f'Arean är cirka {a}')
 # Skriv om programmet för att lösa följande uppgifter.
 # Implementationen ovan ger ett inte så särkillt noggrant svar. 
 # Uppgift 1: Uppatera programmet så du får en area som har 4 decimalers noggranhet.
+# Uppgift 2: Funktionen g(x)=(1-x^2)^0.5 är en halvcirkel. 
+#            Använd numerisk integration för att bestämma ett värde på pi.
+# Uppgift 3: Ibland vill man ha en viss noggranhet, ex 2 eller 8 decimaler. 
+#            Uppdatera programet så kan bestämma arean med ett givet antal värdesiffror.
